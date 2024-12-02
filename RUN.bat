18a3ade1ed499b39237777a99a189734ef46f2cc
Jeffery Lin - TW (SW) <JefferyL@supermicro.com>
2024-12-02 17:12:36 +0800

    Enhance Run.bat

--- RUN.bat
+++ RUN.bat
@@ -1,9 +1,9 @@
 @echo off
-rem 執行 CMake 配置
-cmake -S . -B build
+rem 執行 CMake 配置，生成詳細的 Makefile 或建構系統
+cmake -S . -B build -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
 
-rem 執行 CMake 編譯
-cmake --build build
+rem 執行 CMake 編譯，啟用詳細輸出
+cmake --build build --verbose
 
 rem 運行生成的可執行文件
 build\Debug\LCC.exe
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "Tool/tools.h"
 
 int main() {
